62341e660a51938231f45769cafc01a6401136cd
Jefferson Sousa <jefferson.mello8@hotmail.com>
2023-02-14 13:31:34 -0300

    implementando a base dos outros recursos do endpoint de produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -3,35 +3,50 @@ const router = express.Router();
 const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Retorna todos os produtos'
+
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({ error: error})}
+
+        conn.query(
+            'SELECT * FROM produtos',
+            (error, resultado, fields) => {
+                conn.release();
+                if (error) { return res.status(500).send({ mensagem: 'Ocorreu um erro :/', error: error})}
+
+                return res.status(200).send({response: resultado})
+            }
+        )
     });
 });
 
 router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto;
-    res.status(200).send({
-        id: id,
-        mensagem: 'Retorna detalhes de um produto específico'
+    
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({ error: error})}
+
+        conn.query(
+            'SELECT * FROM produtos WHERE id_produto = ?',
+            [req.params.id_produto],
+            (error, resultado, fields) => {
+                conn.release();
+                
+                if (error) { return res.status(500).send({ mensagem: 'Ocorreu um erro :/', error: error})}
+                return res.status(200).send({response: resultado})
+            }
+        )
     });
 });
 
 router.post('/', (req, res, next) => {
 
     mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error})}
         conn.query(
             'INSERT INTO produtos (nome, preco) VALUES (?,?)',
             [req.body.nome, req.body.preco],
-            (error, resultado, field) => {
+            (error, resultado, fields) => {
                 conn.release();
-
-                if (error) {
-                    return res.status(500).send({
-                        mensagem: 'Ocorreu um erro :/',
-                        error: error,
-                        response: null
-                    });
-                }
+                if (error) { return res.status(500).send({ mensagem: 'Ocorreu um erro :/', error: error})}
 
                 res.status(201).send({
                     mensagem: 'Produto inserido com sucesso',
@@ -43,13 +58,45 @@ router.post('/', (req, res, next) => {
 });
 
 router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Atualiza um produto'
+    
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({ error: error})}
+
+        conn.query(
+            `UPDATE produtos
+                SET nome        = ?,
+                    preco       = ?
+              WHERE id_produto  = ?`,
+            [req.body.nome, req.body.preco, req.body.id_produto],
+            (error, resultado, fields) => {
+                conn.release();
+                if (error) { return res.status(500).send({ mensagem: 'Ocorreu um erro :/', error: error})}
+
+                return res.status(202).send({
+                    mensagem: 'Produto atualizado com sucesso'
+                })
+            }
+        )
     });
 });
 
 router.delete('/', (req, res, next) => {
-    res.status(204).send({
+    
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({ error: error})}
+
+        conn.query(
+            'DELETE FROM produtos WHERE id_produto = ?',
+            [req.body.id_produto],
+            (error, resultado, fields) => {
+                conn.release();
+                if (error) { return res.status(500).send({ mensagem: 'Ocorreu um erro :/', error: error})}
+
+                return res.status(202).send({
+                    mensagem: 'Produto removido com sucesso'
+                })
+            }
+        )
     });
 });
 
